77f84e19e2d6b469813422636c97de246e6615b1
Brit Wallace <britwallace888@gmail.com>
2022-03-25 16:08:14 -0700

    Add database

---
+++ Factory/Migrations/20220325230053_Initial.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    EngineerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.EngineerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.MachineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryItemTags",
+                columns: table => new
+                {
+                    EngineerMachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MachineId = table.Column<int>(type: "int", nullable: false),
+                    EngineerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryItemTags", x => x.EngineerMachineId);
+                    table.ForeignKey(
+                        name: "FK_CategoryItemTags_Categories_EngineerId",
+                        column: x => x.EngineerId,
+                        principalTable: "Categories",
+                        principalColumn: "EngineerId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CategoryItemTags_Items_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Items",
+                        principalColumn: "MachineId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryItemTags_EngineerId",
+                table: "CategoryItemTags",
+                column: "EngineerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryItemTags_MachineId",
+                table: "CategoryItemTags",
+                column: "MachineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryItemTags");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
